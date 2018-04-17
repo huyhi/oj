018d61ad5e826350ee38ee88ffe5e07e4d83bbd9
sky_origin <1062156015@qq.com>
2018-04-17 18:28:49 +0800

    add teacher and student page

--- sign/urls.py
+++ sign/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.teacher_index, name='sign.teacher.index'),
-    url(r'^$', views.student_index, name='sign.student.index'),
+    url(r'^sign/teacher_sign$', views.teacher_index, name='sign.teacher.index'),
+    url(r'^sign/student_sign$', views.student_index, name='sign.student.index'),
     url(r'^start_sign_prj$', views.startSign, name='sign.start')
 ]
